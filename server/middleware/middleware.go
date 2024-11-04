847c5f3bd6774f430da6e712723a9cc75619f907
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-11-04 22:17:25 +0900

    middleware database connection check

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,25 +1,23 @@
 package middleware
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	// 	"net/http"
+	// 	"github.com/gorilla/mux"
+	// "go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson/primitive"
+	// "go.mongodb.org/mongo-driver/mongo"
+	// "go.mongodb.org/mongo-driver/mongo/options"
 )
-
-// import (
-// 	"context"
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"os"
-// 	"github.com/gorilla/mux"
-// "go.mongodb.org/mongo-driver/bson"
-// "go.mongodb.org/mongo-driver/bson/primitive"
-// "go.mongodb.org/mongo-driver/mongo"
-// "go.mongodb.org/mongo-driver/mongo/options"
-// "github.com/joho/godotenv"
-// )
 
 var collection *mongo.Collection
 
@@ -35,11 +33,38 @@ func loadTheEnv() {
 	}
 }
 
-func GetAllTasks() {
+func createDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collName := os.Getenv("DB_COLLECTION_NAME")
 
+	clientOptions := options.Client().ApplyURI(connectionString)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to mongodb")
+
+	collection = client.Database(dbName).Collection(collName)
+	fmt.Println("collection instance created")
 }
 
-func CreateTask() {
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+	//このヘッダーのキーがContent-Typeかもしれない
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// このgetAllTasks関数どこから来てんねん
+	payload := getAllTasks()
+	json.NewEncoder(w).Encode(payload)
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
 
 }
 
