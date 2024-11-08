55034d5d7151c838329cb5b4f58246e1abae07eb
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-11-09 04:10:18 +0900

    without CORS

--- server/main.go
+++ server/main.go
@@ -6,19 +6,11 @@ import (
 	"net/http"
 
 	"github.com/Kumoichi/golang-react-todo/router"
-	"github.com/gorilla/handlers"
 )
 
 func main() {
 	r := router.Router()
 	fmt.Println("starting the server on port 9000")
 
-	// CORS設定を追加
-	// "*"はすべてのオリジンを許可しますが、特定のオリジン（例: "http://localhost:3000"）も指定可能です
-	corsAllowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000"})
-	corsAllowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
-	corsAllowedHeaders := handlers.AllowedHeaders([]string{"Content-Type"})
-
-	// CORSミドルウェアをサーバーに追加して開始
-	log.Fatal(http.ListenAndServe(":9000", handlers.CORS(corsAllowedOrigins, corsAllowedMethods, corsAllowedHeaders)(r)))
+	log.Fatal(http.ListenAndServe(":9000", r))
 }
--- server/router/router.go
+++ server/router/router.go
@@ -1,33 +1,17 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/Kumoichi/golang-react-todo/middleware"
 	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 )
 
-// Router sets up routes and applies CORS settings
-func Router() http.Handler {
+func Router() *mux.Router {
 	router := mux.NewRouter()
-
-	// Define the routes
 	router.HandleFunc("/api/task", middleware.GetAllTasks).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/tasks", middleware.CreateTask).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/tasks/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
-	router.HandleFunc("/api/deleteAllTasks", middleware.DeleteAllTasks).Methods("DELETE", "OPTIONS")
-
-	// Set up CORS with the necessary settings
-	corsHandler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"}, // ReactアプリのURLを指定
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},
-		AllowCredentials: true,
-	})
-
-	// Return the router wrapped with the CORS handler
-	return corsHandler.Handler(router)
+	router.HandleFunc("/api/dleteAllTasks", middleware.DeleteAllTasks).Methods("DELETE", "OPTIONS")
+	return router
 }
