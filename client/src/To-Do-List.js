b4ba64578eab1f36865db24ae6e908247073cb05
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-12-01 23:31:24 +0900

    format change

--- client/src/To-Do-List.js
+++ client/src/To-Do-List.js
@@ -1,187 +1,140 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Card, Header, Form, Input, Icon } from 'semantic-ui-react';
 
 let endpoint = 'http://localhost:9000';
 
-class ToDoList extends Component {
-  constructor(props) {
-    super(props);
+function ToDoList() {
+  const [task, setTask] = useState('');
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    this.state = {
-      task: '',
-      items: [],
-      loading: false, // Added loading state
-    };
-  }
+  useEffect(() => {
+    getTask();
+  }, []);
 
-  componentDidMount() {
-    this.getTask();
-  }
-
-  onChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
+  const onChange = (event) => {
+    setTask(event.target.value);
   };
 
-  onSubmit = () => {
-    let { task } = this.state;
-
+  const onSubmit = () => {
     if (task) {
-      this.setState({ loading: true }); // Start loading
-
+      setLoading(true);
       axios
-        .post(
-          endpoint + '/api/tasks',
-          { task },
-          {
-            headers: {
-              'Content-Type': 'application/json', // Corrected header
-            },
-          }
-        )
-        .then((res) => {
-          this.getTask();
-          this.setState({
-            task: '',
-            loading: false, // End loading
-          });
-          console.log(res);
+        .post(endpoint + '/api/tasks', { task }, {
+            //content-typeでjson形式でサーバーにデータ尾を送るというのを伝えている
+          headers: { 'Content-Type': 'application/json' }
+        })
+        .then(() => {
+          getTask();
+          setTask('');
+          setLoading(false);
         })
         .catch((err) => {
           console.error('Error creating task:', err);
-          this.setState({ loading: false }); // End loading on error
+          setLoading(false);
         });
     }
   };
-
-  getTask = () => {
-    this.setState({ loading: true }); // Start loading
-
+//todo shihota ここから復習
+  const getTask = () => {
+    setLoading(true);
     axios
       .get(endpoint + '/api/task')
       .then((res) => {
         if (res.data) {
-          this.setState({
-            items: res.data.map((item) => {
-              let color = 'yellow';
-              let style = {
+          setItems(
+            res.data.map((item) => ({
+              ...item,
+              color: item.status ? 'green' : 'yellow',
+              style: {
                 wordWrap: 'break-word',
-              };
-              if (item.status) {
-                color = 'green';
-                style['textDecorationLine'] = 'line-through';
-              }
-              return (
-                <Card key={item._id} color={color} fluid className="rough">
-                  <Card.Content>
-                    <Card.Header textAlign="left">
-                      <div style={style}>{item.task}</div>
-                    </Card.Header>
-                  {/* </Card.Content> */}
+                textDecorationLine: item.status ? 'line-through' : 'none',
+              },
+            }))
+          );
+        } else {
+          setItems([]);
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error('Error fetching tasks:', err);
+        setLoading(false);
+      });
+  };
 
+  const updateTask = (id, status) => {
+    axios
+      .put(endpoint + '/api/tasks/' + id, { status }, {
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then(() => getTask())
+      .catch((err) => console.error('Error updating task:', err));
+  };
+
+  const deleteTask = (id) => {
+    axios
+      .delete(endpoint + '/api/deleteTask/' + id, {
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then(() => getTask())
+      .catch((err) => console.error('Error deleting task:', err));
+  };
+
+  return (
+    <div>
+      <div className="row">
+        <Header className="header" as="h2" color="yellow">
+          To Do List
+        </Header>
+      </div>
+      <div className="row">
+        <Form onSubmit={onSubmit}>
+          <Input
+            type="text"
+            name="task"
+            onChange={onChange}
+            value={task}
+            fluid
+            placeholder="Create Task"
+            disabled={loading}
+          />
+        </Form>
+      </div>
+      <div className="row">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <Card.Group>
+            {items.map((item) => (
+              <Card key={item._id} color={item.color} fluid>
+                <Card.Content>
+                  <Card.Header textAlign="left">
+                    <div style={item.style}>{item.task}</div>
+                  </Card.Header>
                   <Card.Meta textAlign="right">
                     <Icon
                       name="check circle"
                       color="blue"
-                      onClick={() => this.updateTask(item._id, !item.status)} // Toggle task status
+                      onClick={() => updateTask(item._id, !item.status)}
                     />
                     <span style={{ paddingRight: 10 }}>Undo</span>
                     <Icon
                       name="delete"
                       color="red"
-                      onClick={() => this.deleteTask(item._id)}
+                      onClick={() => deleteTask(item._id)}
                     />
                     <span style={{ paddingRight: 10 }}>Delete</span>
                   </Card.Meta>
-                  </Card.Content>
-                </Card>
-              );
-            }),
-            loading: false, // End loading
-          });
-        } else {
-          this.setState({
-            items: [],
-            loading: false, // End loading
-          });
-        }
-      })
-      .catch((err) => {
-        console.error('Error fetching tasks:', err);
-        this.setState({ loading: false }); // End loading on error
-      });
-  };
-
-  updateTask = (id, status) => {
-    axios
-      .put(
-        endpoint + '/api/tasks/' + id,
-        { status },
-        {
-          headers: {
-            'Content-Type': 'application/json', // Corrected header
-          },
-        }
-      )
-      .then((res) => {
-        console.log(res);
-        this.getTask();
-      })
-      .catch((err) => {
-        console.error('Error updating task:', err);
-      });
-  };
-
-  deleteTask = (id) => {
-    axios
-      .delete(endpoint + '/api/deleteTask/' + id, {
-        headers: {
-          'Content-Type': 'application/json', // Corrected header
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        this.getTask();
-      })
-      .catch((err) => {
-        console.error('Error deleting task:', err);
-      });
-  };
-
-  render() {
-    return (
-      <div>
-        <div className="row">
-          <Header className="header" as="h2" color="yellow">
-            To Do List
-          </Header>
-        </div>
-        <div className="row">
-          <Form onSubmit={this.onSubmit}>
-            <Input
-              type="text"
-              name="task"
-              onChange={this.onChange}
-              value={this.state.task}
-              fluid
-              placeholder="Create Task"
-              disabled={this.state.loading} // Disable input when loading
-            />
-            {/* <Button> Create Task </Button> */}
-          </Form>
-        </div>
-        <div className="row">
-          {this.state.loading ? (
-            <p>Loading...</p> // Display loading message while fetching data
-          ) : (
-            <Card.Group>{this.state.items}</Card.Group>
-          )}
-        </div>
+                </Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ToDoList;
